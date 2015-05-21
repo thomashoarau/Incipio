f9d70a0eb056597df889c8d22636ce10df32f671
ThÃ©o FIDRY <theo.fidry@gmail.com>
2015-05-21 23:43:40 +0200

    Added Scrutinizer and fixed syntax.

--- src/ApiBundle/Controller/MandateController.php
+++ src/ApiBundle/Controller/MandateController.php
@@ -12,10 +12,8 @@
 namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * Class MandateController.
@@ -35,6 +33,7 @@ class MandateController extends Controller
     {
         $test = $this->getDoctrine()->getManager()->getRepository('ApiBundle:Mandate')->findCurrent();
 
-        dump($test);die();
+        dump($test);
+        die();
     }
 }
