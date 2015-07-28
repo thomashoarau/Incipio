e2bd7909bdfc47f7bb62c55d49e097f10912a3ee
ThÃ©o FIDRY <theo.fidry@gmail.com>
2015-07-28 11:37:39 +0200

    Set UTC time everywhere

---
+++ src/ApiBundle/Doctrine/DBAL/Type/UTCDateTimeType.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Incipio package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ApiBundle\Doctrine\DBAL\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateTimeType;
+
+/**
+ * Class UTCDateTimeType: used to set the timezone of any date to UTC.
+ *
+ * @link https://github.com/Atlantic18/DoctrineExtensions/blob/master/doc/timestampable.md#creating-a-utc-datetime-type-that-stores-your-datetimes-in-utc
+ */
+class UTCDateTimeType extends DateTimeType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($phpValue, AbstractPlatform $platform)
+    {
+        if ($phpValue instanceof \DateTime) {
+            $phpValue->setTimeZone(new \DateTimeZone('UTC'));
+        }
+
+        return parent::convertToDatabaseValue($phpValue, $platform);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($databaseValue, AbstractPlatform $platform)
+    {
+        $phpValue = parent::convertToPHPValue($databaseValue, $platform);
+
+        if ($phpValue instanceof \DateTime) {
+            $phpValue->setTimeZone(new \DateTimeZone('UTC'));
+        }
+
+        return $phpValue;
+    }
+}
