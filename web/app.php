674c700e3abbf5093048ba997bc186aca96210df
stoakes <Stoakes@users.noreply.github.com>
2017-06-03 15:28:22 +0200

    Update to Symfony 3.3

--- web/app.php
+++ web/app.php
@@ -1,21 +1,10 @@
 <?php
 
-/* get Symfony\Component\ClassLoader\ApcClassLoader;*/
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
-
-/** Use APC for autoloading to improve performance
- * Change 'sf2' by the prefix you want in order to prevent key conflict with another application
- * loader = new ApcClassLoader('sf2', $loader);
- * loader->register(true);
-*/
-
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-/* kernel = new AppCache($kernel);*/
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
