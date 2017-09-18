99c4d339b6290134bfc4a3293553af2d778b1298
stoakes <Stoakes@users.noreply.github.com>
2017-09-18 19:46:34 +0200

    Remove unused actions and templates about ProcesVerbal
    Use Paramconverter for ProcesVerbalController
    
    Ping #53

--- src/Mgate/SuiviBundle/Controller/ProcesVerbalController.php
+++ src/Mgate/SuiviBundle/Controller/ProcesVerbalController.php
@@ -18,59 +18,24 @@ use Mgate\SuiviBundle\Form\Type\ProcesVerbalType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProcesVerbalController extends Controller
 {
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     *
+     * @param Request $request
+     * @param Etude   $etude
+     *
+     * @return RedirectResponse|Response
      */
-    public function indexAction($page)
+    public function addAction(Request $request, Etude $etude)
     {
         $em = $this->getDoctrine()->getManager();
-
-        $entities = $em->getRepository('MgateSuiviBundle:Etude')->findAll();
-
-        return $this->render('MgateSuiviBundle:Etude:index.html.twig', [
-            'etudes' => $entities,
-        ]);
-    }
-
-    /**
-     * @Security("has_role('ROLE_SUIVEUR')")
-     */
-    public function voirAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('MgateSuiviBundle:ProcesVerbal')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find ProcesVerbal entity.');
-        }
-
-        $etude = $entity->getEtude();
-
-        if ($this->get('Mgate.etude_manager')->confidentielRefus($etude, $this->getUser(), $this->get('security.authorization_checker'))) {
-            throw new AccessDeniedException('Cette étude est confidentielle');
-        }
-
-        return $this->render('MgateSuiviBundle:ProcesVerbal:voir.html.twig', [
-            'procesverbal' => $entity,
-        ]);
-    }
-
-    /**
-     * @Security("has_role('ROLE_SUIVEUR')")
-     */
-    public function addAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        if (!$etude = $em->getRepository('Mgate\SuiviBundle\Entity\Etude')->find($id)) {
-            throw $this->createNotFoundException('L\'étude n\'existe pas !');
-        }
 
         if ($this->get('Mgate.etude_manager')->confidentielRefus($etude, $this->getUser(), $this->get('security.authorization_checker'))) {
             throw new AccessDeniedException('Cette étude est confidentielle');
@@ -98,14 +63,15 @@ class ProcesVerbalController extends Controller
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     *
+     * @param Request      $request
+     * @param ProcesVerbal $procesverbal
+     *
+     * @return RedirectResponse|Response
      */
-    public function modifierAction(Request $request, $id_pv)
+    public function modifierAction(Request $request, ProcesVerbal $procesverbal)
     {
         $em = $this->getDoctrine()->getManager();
-
-        if (!$procesverbal = $em->getRepository('Mgate\SuiviBundle\Entity\ProcesVerbal')->find($id_pv)) {
-            throw $this->createNotFoundException('Le Procès Verbal n\'existe pas !');
-        }
 
         $etude = $procesverbal->getEtude();
 
@@ -140,9 +106,9 @@ class ProcesVerbalController extends Controller
      *
      * @param Request $request
      * @param Etude   $etude
-     * @param $type string PVR or PVRI
+     * @param string  $type    PVR or PVRI
      *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @return RedirectResponse|Response
      */
     public function redigerAction(Request $request, Etude $etude, $type)
     {
@@ -180,26 +146,27 @@ class ProcesVerbalController extends Controller
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
+     *
+     * @param Request      $request
+     * @param ProcesVerbal $procesVerbal
+     *
+     * @return RedirectResponse
      */
-    public function deleteAction(Request $request, $id_pv)
+    public function deleteAction(Request $request, ProcesVerbal $procesVerbal)
     {
-        $form = $this->createDeleteForm($id_pv);
+        $form = $this->createDeleteForm($procesVerbal->getId());
         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
-            if (!$entity = $em->getRepository('Mgate\SuiviBundle\Entity\ProcesVerbal')->find($id_pv)) {
-                throw $this->createNotFoundException('Le Procès Verbal n\'existe pas !');
-            }
-
-            $etude = $entity->getEtude();
+            $etude = $procesVerbal->getEtude();
 
             if ($this->get('Mgate.etude_manager')->confidentielRefus($etude, $this->getUser(), $this->get('security.authorization_checker'))) {
                 throw new AccessDeniedException('Cette étude est confidentielle');
             }
 
-            $em->remove($entity);
+            $em->remove($procesVerbal);
             $em->flush();
         }
 
