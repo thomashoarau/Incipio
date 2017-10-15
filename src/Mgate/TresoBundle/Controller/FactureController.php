5577e1995cd81d9d51dc085ee27d143b9831906a
stoakes <Stoakes@users.noreply.github.com>
2017-10-15 07:47:09 +0200

    Split Facture management into 2 methods
    Create a method to initialize a facture
    
    One to add, one to edit; rather than a big one for both with default parameters and an unclear interface

--- src/Mgate/TresoBundle/Controller/FactureController.php
+++ src/Mgate/TresoBundle/Controller/FactureController.php
@@ -11,6 +11,8 @@
 
 namespace Mgate\TresoBundle\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use Mgate\SuiviBundle\Entity\Etude;
 use Mgate\SuiviBundle\Entity\Phase;
 use Mgate\TresoBundle\Entity\Facture as Facture;
 use Mgate\TresoBundle\Entity\FactureDetail as FactureDetail;
@@ -56,89 +58,15 @@ class FactureController extends Controller
      * @Security("has_role('ROLE_TRESO')")
      *
      * @param Request $request
-     * @param $id
-     * @param $etude_id
+     * @param Etude   $etude Etude to which the Facture will be added
      *
      * @return RedirectResponse|Response
      */
-    public function modifierAction(Request $request, $id, $etude_id)
+    public function ajouterAction(Request $request, ?Etude $etude)
     {
         $em = $this->getDoctrine()->getManager();
-        $keyValueStore = $this->get('app.json_key_value_store');
-        if (!$keyValueStore->exists('tva')) {
-            throw new \RuntimeException('Le paramètres tva n\'est pas disponible.');
-        }
-        $tauxTVA = 100 * $keyValueStore->get('tva'); // former value: 20, tva is stored as 0.2 in key-value store
-        $compteEtude = 705000;
-        $compteFrais = 708500;
-        $compteAcompte = 419100;
-        if ($keyValueStore->exists('namingConvention')) {
-            $namingConvention = $keyValueStore->get('namingConvention');
-        } else {
-            $namingConvention = 'id';
-        }
 
-        if (!$facture = $em->getRepository('MgateTresoBundle:Facture')->find($id)) {
-            $facture = new Facture();
-            $now = new \DateTime('now');
-            $facture->setDateEmission($now);
-
-            if ($etude = $em->getRepository('MgateSuiviBundle:Etude')->find($etude_id)) {
-                $formater = $this->container->get('Mgate.conversionlettre');
-
-                $facture->setEtude($etude);
-                $facture->setBeneficiaire($etude->getProspect());
-
-                if (!count($etude->getFactures()) && $etude->getAcompte()) {
-                    $facture->setType(Facture::TYPE_VENTE_ACCOMPTE);
-                    $facture->setObjet('Facture d\'acompte sur l\'étude ' . $etude->getReference($namingConvention) . ', correspondant au règlement de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) . ' % de l’étude.');
-                    $detail = new FactureDetail();
-                    $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteAcompte]));
-                    $detail->setFacture($facture);
-                    $facture->addDetail($detail);
-                    $detail->setDescription('Acompte de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) . ' % sur l\'étude ' . $etude->getReference());
-                    $detail->setMontantHT($etude->getPourcentageAcompte() * $etude->getMontantHT());
-                    $detail->setTauxTVA($tauxTVA);
-                } else {
-                    $facture->setType(Facture::TYPE_VENTE_SOLDE);
-                    if ($etude->getAcompte() && $etude->getFa()) {
-                        $montantADeduire = new FactureDetail();
-                        $montantADeduire->setDescription('Facture d\'acompte sur l\'étude ' . $etude->getReference($namingConvention) .
-                            ', correspondant au règlement de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) .
-                            ' % de l’étude.')->setFacture($facture);
-                        $facture->setMontantADeduire($montantADeduire);
-                    }
-
-                    $totalTTC = 0;
-                    /** @var Phase $phase */
-                    foreach ($etude->getPhases() as $phase) {
-                        $detail = new FactureDetail();
-                        $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteEtude]));
-                        $detail->setFacture($facture);
-                        $facture->addDetail($detail);
-                        $detail->setDescription('Phase ' . ($phase->getPosition() + 1) . ' : ' . $phase->getTitre() . ' : ' .
-                            $phase->getNbrJEH() . ' JEH * ' . $formater->moneyFormat($phase->getPrixJEH()) . ' €');
-                        $detail->setMontantHT($phase->getPrixJEH() * $phase->getNbrJEH());
-                        $detail->setTauxTVA($tauxTVA);
-
-                        $totalTTC += $phase->getPrixJEH() * $phase->getNbrJEH();
-                    }
-                    $detail = new FactureDetail();
-                    $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteFrais]))
-                        ->setFacture($facture)
-                        ->setDescription('Frais de dossier')
-                        ->setMontantHT($etude->getFraisDossier());
-                    $facture->addDetail($detail);
-                    $detail->setTauxTVA($tauxTVA);
-
-                    $totalTTC += $etude->getFraisDossier();
-                    $totalTTC *= (1 + $tauxTVA / 100);
-
-                    $facture->setObjet('Facture de Solde sur l\'étude ' . $etude->getReference($namingConvention) . '.');
-                }
-            }
-        }
-
+        $facture = $this->createFacture($em, $etude);
         $form = $this->createForm(FactureType::class, $facture);
 
         if ($request->getMethod() == 'POST') {
@@ -151,10 +79,54 @@ class FactureController extends Controller
 
                 if ($facture->getType() <= Facture::TYPE_VENTE_ACCOMPTE || $facture->getMontantADeduire() === null || $facture->getMontantADeduire()->getMontantHT() == 0) {
                     $facture->setMontantADeduire(null);
+                } else {
+                    $facture->getMontantADeduire()->setFactureADeduire($facture);
                 }
 
                 $em->persist($facture);
                 $em->flush();
+                $this->addFlash('success', 'Facture ajoutée');
+
+                return $this->redirect($this->generateUrl('MgateTreso_Facture_voir', ['id' => $facture->getId()]));
+            }
+            $this->addFlash('danger', 'Le formulaire contient des erreurs.');
+        }
+
+        return $this->render('MgateTresoBundle:Facture:modifier.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Security("has_role('ROLE_TRESO')")
+     *
+     * @param Request $request
+     * @param Facture $facture
+     *
+     * @return RedirectResponse|Response
+     */
+    public function modifierAction(Request $request, Facture $facture)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(FactureType::class, $facture);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                foreach ($facture->getDetails() as $factured) {
+                    $factured->setFacture($facture);
+                }
+
+                if ($facture->getType() <= Facture::TYPE_VENTE_ACCOMPTE || $facture->getMontantADeduire() === null ||
+                    $facture->getMontantADeduire()->getMontantHT() == 0
+                ) {
+                    $facture->setMontantADeduire(null);
+                }
+
+                $em->persist($facture);
+                $em->flush();
+                $this->addFlash('success', 'Facture modifiée');
 
                 return $this->redirect($this->generateUrl('MgateTreso_Facture_voir', ['id' => $facture->getId()]));
             }
@@ -206,5 +178,87 @@ class FactureController extends Controller
             ->setmethod('DELETE')
             ->setAction($this->generateUrl('MgateTreso_Facture_supprimer', ['id' => $facture->getId()]))
             ->getForm();
+    }
+
+    /**
+     * Returns a well formatted facture, according to current Etude state.
+     *
+     * @param ObjectManager $em
+     * @param Etude         $etude
+     *
+     * @return Facture
+     */
+    private function createFacture(ObjectManager $em, ?Etude $etude)
+    {
+        $keyValueStore = $this->get('app.json_key_value_store');
+        if (!$keyValueStore->exists('tva')) {
+            throw new \RuntimeException('Le paramètres tva n\'est pas disponible.');
+        }
+        $tauxTVA = 100 * $keyValueStore->get('tva'); // former value: 20, tva is stored as 0.2 in key-value store
+        $compteEtude = 705000;
+        $compteFrais = 708500;
+        $compteAcompte = 419100;
+        if ($keyValueStore->exists('namingConvention')) {
+            $namingConvention = $keyValueStore->get('namingConvention');
+        } else {
+            $namingConvention = 'id';
+        }
+        $facture = new Facture();
+        $now = new \DateTime('now');
+        $facture->setDateEmission($now);
+
+        $formater = $this->container->get('Mgate.conversionlettre');
+
+        if ($etude) {
+            $facture->setEtude($etude);
+            $facture->setBeneficiaire($etude->getProspect());
+        }
+
+        if ($etude && !count($etude->getFactures()) && $etude->getAcompte()) {
+            $facture->setType(Facture::TYPE_VENTE_ACCOMPTE);
+            $facture->setObjet('Facture d\'acompte sur l\'étude ' . $etude->getReference($namingConvention) . ', correspondant au règlement de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) . ' % de l’étude.');
+            $detail = new FactureDetail();
+            $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteAcompte]));
+            $detail->setFacture($facture);
+            $facture->addDetail($detail);
+            $detail->setDescription('Acompte de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) . ' % sur l\'étude ' . $etude->getReference());
+            $detail->setMontantHT($etude->getPourcentageAcompte() * $etude->getMontantHT());
+            $detail->setTauxTVA($tauxTVA);
+        } else {
+            $facture->setType(Facture::TYPE_VENTE_SOLDE);
+            if ($etude && $etude->getAcompte() && $etude->getFa()) {
+                $montantADeduire = new FactureDetail();
+                $montantADeduire->setDescription('Facture d\'acompte sur l\'étude ' . $etude->getReference($namingConvention) .
+                    ', correspondant au règlement de ' . $formater->moneyFormat(($etude->getPourcentageAcompte() * 100)) .
+                    ' % de l’étude.')
+                    ->setFactureADeduire($facture);
+                $facture->setMontantADeduire($montantADeduire);
+            }
+
+            if ($etude) {
+                /** @var Phase $phase */
+                foreach ($etude->getPhases() as $phase) {
+                    $detail = new FactureDetail();
+                    $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteEtude]));
+                    $detail->setFacture($facture);
+                    $facture->addDetail($detail);
+                    $detail->setDescription('Phase ' . ($phase->getPosition() + 1) . ' : ' . $phase->getTitre() . ' : ' .
+                        $phase->getNbrJEH() . ' JEH * ' . $formater->moneyFormat($phase->getPrixJEH()) . ' €');
+                    $detail->setMontantHT($phase->getPrixJEH() * $phase->getNbrJEH());
+                    $detail->setTauxTVA($tauxTVA);
+
+                }
+                $detail = new FactureDetail();
+                $detail->setCompte($em->getRepository('MgateTresoBundle:Compte')->findOneBy(['numero' => $compteFrais]))
+                    ->setFacture($facture)
+                    ->setDescription('Frais de dossier')
+                    ->setMontantHT($etude->getFraisDossier());
+                $facture->addDetail($detail);
+                $detail->setTauxTVA($tauxTVA);
+                $facture->setObjet('Facture de Solde sur l\'étude ' . $etude->getReference($namingConvention) . '.');
+            }
+        }
+
+        return $facture;
     }
 }
